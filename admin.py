4ba66a96e0eb8125960e31c01980256f2ce5dad8
StarStream <alexiswiesen@yahoo.com>
2025-10-28 18:09:03 +0100

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,26 +1,27 @@
 from quart import Blueprint, render_template
-admin = Blueprint("admin", __name__)
 
-@admin.route("/dashboard")
+admin_bp = Blueprint("admin", __name__)
+
+@admin_bp.route("/dashboard")
 async def admin_dashboard():
     return await render_template("admin_dashboard.html")
 
-@admin.route("/add")
+@admin_bp.route("/add")
 async def add_card():
     return await render_template("add_card.html")
 
-@admin.route("/history")
+@admin_bp.route("/history")
 async def history():
     return await render_template("history.html")
 
-@admin.route("/edit")
+@admin_bp.route("/edit")
 async def edit_card_list():
     return await render_template("edit_card_list.html")
 
-@admin.route("/search")
+@admin_bp.route("/search")
 async def search_player():
     return await render_template("search_player.html")
 
-@admin.route("/manage")
+@admin_bp.route("/manage")
 async def manage():
     return await render_template("manage.html")
