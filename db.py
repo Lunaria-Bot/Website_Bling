59ccbfe41f9ada027e788e97267cdb721c225ebd
StarStream <alexiswiesen@yahoo.com>
2025-10-28 15:20:41 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,8 @@
 import asyncpg
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 db_pool = None
 
 async def init_db():
     global db_pool
-    db_pool = await asyncpg.create_pool(dsn=os.getenv("DATABASE_URL"))
+    db_pool = await asyncpg.create_pool(dsn=os.environ["DATABASE_URL"])
