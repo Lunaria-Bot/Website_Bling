21a19d7fe82c44ad2294dd5f5d41d9f9edf9edcf
StarStream <alexiswiesen@yahoo.com>
2025-10-28 18:04:32 +0100

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,37 +1,26 @@
-from quart import Blueprint, render_template, session, redirect, url_for
+from quart import Blueprint, render_template
+admin = Blueprint("admin", __name__)
 
-admin_bp = Blueprint("admin", __name__)
-
-def require_admin():
-    if not session.get("is_admin"):
-        return redirect(url_for("home"))
-
-@admin_bp.route("/admin/dashboard")
+@admin.route("/dashboard")
 async def admin_dashboard():
-    if not session.get("is_admin"):
-        return redirect(url_for("home"))
-    return await render_template("admin/dashboard.html")
+    return await render_template("admin_dashboard.html")
 
-@admin_bp.route("/admin/add_card")
+@admin.route("/add")
 async def add_card():
-    if not session.get("is_admin"):
-        return redirect(url_for("home"))
-    return await render_template("admin/add_card.html")
+    return await render_template("add_card.html")
 
-@admin_bp.route("/admin/edit_card_list")
+@admin.route("/history")
+async def history():
+    return await render_template("history.html")
+
+@admin.route("/edit")
 async def edit_card_list():
-    if not session.get("is_admin"):
-        return redirect(url_for("home"))
-    return await render_template("admin/edit_card_list.html")
+    return await render_template("edit_card_list.html")
 
-@admin_bp.route("/admin/search_player")
+@admin.route("/search")
 async def search_player():
-    if not session.get("is_admin"):
-        return redirect(url_for("home"))
-    return await render_template("admin/search_player.html")
+    return await render_template("search_player.html")
 
-@admin_bp.route("/admin/manage")
+@admin.route("/manage")
 async def manage():
-    if not session.get("is_admin"):
-        return redirect(url_for("home"))
-    return await render_template("admin/manage.html")
+    return await render_template("manage.html")
