053585f0f1f622816b7dd9bc80c4be842b448062
StarStream <alexiswiesen@yahoo.com>
2025-10-28 15:20:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
 from quart import Quart, render_template, session
-from auth import auth_bp
-from player import player_bp
+from authy import authy
 from admin import admin_bp
-import os
-from dotenv import load_dotenv
-from db import init_db, db_pool
 from player import player_bp
+from db import init_db, db_pool
 
-
-load_dotenv()
+import os
 
 app = Quart(__name__, template_folder="templates", static_folder="static")
-app.secret_key = os.getenv("SECRET_KEY", "warp-gate-secret")
+app.secret_key = os.environ["SECRET_KEY"]
 
-# Blueprints
-app.register_blueprint(auth_bp)
-app.register_blueprint(player_bp)
+# Register blueprints
+app.register_blueprint(authy)
 app.register_blueprint(admin_bp)
 app.register_blueprint(player_bp)
-
-@app.route("/")
-async def home():
-    return await render_template("base.html")
 
 @app.before_serving
 async def startup():
     await init_db()
-    player_bp.db_pool = db_pool  # injecte le pool dans player.py
+    player_bp.db_pool = db_pool  # inject pool into player
+    admin_bp.db_pool = db_pool   # optional: if admin needs DB
+
+@app.route("/")
+async def home():
+    return await render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
